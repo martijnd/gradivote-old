585296f1e1e48820abe1b4d45ad621ede6611aa4
martijnd <martijn.dorsman@gmail.com>
2020-04-08 20:20:44 +0200

    Remove firebase and setup Now

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 export default {
   mode: 'universal',
   /*
@@ -40,41 +42,15 @@ export default {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/firebase'
+    '@nuxtjs/axios'
   ],
 
-  firebase: {
-    services: {
-      auth: {
-        ssr: true
-      }
-    }
-  },
-  pwa: {
-    // disable the modules you don't need
-    meta: false,
-    icon: false,
-    // if you omit a module key form configuration sensible defaults will be applied
-    // manifest: false,
-
-    workbox: {
-      importScripts: [
-        // ...
-        '/firebase-auth-sw.js'
-      ],
-      // by default the workbox module will not install the service worker in dev environment to avoid conflicts with HMR
-      // only set this true for testing and remember to always clear your browser cache in development
-      dev: false
-    }
-  },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'http://localhost:8000/api',
+    baseURL: process.env.API_BASE_URL,
     headers: { 'Content-Type': 'application/json' }
   },
   /*
--- store/index.js
+++ store/index.js
@@ -13,20 +13,6 @@ export const state = () => ({
 });
 
 export const mutations = {
-  ON_AUTH_STATE_CHANGED_MUTATION(state, { authUser, claims }) {
-    // you can request additional fields if they are optional (e.g. photoURL)
-    const { uid, email, emailVerified, displayName, photoURL } = authUser
-
-    state.authUser = {
-      uid,
-      displayName,
-      email,
-      emailVerified,
-      photoURL: photoURL || null, // results in photoURL being null for server auth
-      // use custom claims to control access (see https://firebase.google.com/docs/auth/admin/custom-claims)
-      isAdmin: claims.custom_claim
-    }
-  }
   login(state, user) {
     state.user = user;
     state.loggedIn = true;
@@ -48,27 +34,6 @@ export const mutations = {
 
 
 export const actions = {
-  async onAuthStateChangedAction({ commit, dispatch }, { authUser, claims }) {
-    if (!authUser) {
-      await dispatch('cleanupAction')
-
-      return
-    }
-
-    // you can request additional fields if they are optional (e.g. photoURL)
-    const { uid, email, emailVerified, displayName, photoURL } = authUser
-
-    commit('SET_USER', {
-      uid,
-      email,
-      emailVerified,
-      displayName,
-      photoURL, // results in photoURL being undefined for server auth
-      // use custom claims to control access (see https://firebase.google.com/docs/auth/admin/custom-claims)
-      isAdmin: claims.custom_claim
-    })
-  }
-
 
   async login({ commit, dispatch }, data) {
     const response = await this.$axios({
